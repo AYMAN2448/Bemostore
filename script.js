e2a7eeaac7947fd35dc4cee183feedb51458fb98
AYMAN2448 <aloby2448@gmail.com>
2026-05-22 22:32:53 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+// script.js
+function checkAuth() {
+    const userId = localStorage.getItem('userId');
+    const infoDiv = document.getElementById('user-info');
+    if (userId) {
+        infoDiv.innerHTML = `مرحباً ${localStorage.getItem('userName') || ''} | <button onclick="logout()">تسجيل خروج</button>`;
+    } else {
+        infoDiv.innerHTML = `<a href="login.html" style="color:white;">دخول</a> | <a href="register.html" style="color:white;">تسجيل</a>`;
+    }
+}
+function logout() {
+    localStorage.clear();
+    window.location.href = 'index.html';
+}
